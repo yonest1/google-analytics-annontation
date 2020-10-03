71ba7cf0de2804b8d62358b54e30020b1cf5e385
Yonatan <example@examplemail.com>
2020-10-03 16:43:08 +0300

    successful version

--- main.py
+++ main.py
@@ -1,30 +1,30 @@
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.chrome.options import Options
 from time import sleep
+
+from selenium.webdriver import DesiredCapabilities
+
 from cred import theuser, thepassword
-
-
-#chrome_options = Options()
-#chrome_options.add_argument("--headless")
 
 class Google:
 
     def __init__(self, username, password):
 
         self.driver = webdriver.Chrome()
+        print("opening stackoverflow")
         self.driver.get('https://stackoverflow.com/users/signup?ssrc=head&returnurl=%2fusers%2fstory%2fcurrent%27')
-        sleep(5)
+        #self.driver.get('https://www.evernote.com/Registration.action?referralSpecifier=mktgnwp_en_en_web_hpg_V00&offer=perm_mktgweb_cp_basic')
+        sleep(8)
         self.driver.find_element_by_xpath('//*[@id="openid-buttons"]/button[1]').click()
+        #self.driver.find_element_by_xpath('//*[@id="googleOauthButton"]/div/div').click()
+        print("login with user/pw")
         self.driver.find_element_by_xpath('//input[@type="email"]').send_keys(username)
         self.driver.find_element_by_xpath('//*[@id="identifierNext"]').click()
-        sleep(5)
+        sleep(8)
         self.driver.find_element_by_xpath('//input[@type="password"]').send_keys(password)
         self.driver.find_element_by_xpath('//*[@id="passwordNext"]').click()
         sleep(4)
-        self.driver.get('https://analytics.google.com/analytics/web/#/report-home/a146806591w208716095p201158238')
+        print("switching to analytics")
+        self.driver.get('https://analytics.google.com/analytics/web/provision/#/report-home/a179551378w248167090p230262745/')
         sleep(8)
         self.driver.find_element_by_xpath(
             '//*[@id="reporting"]/div/div/div/ga-nav-link[3]/div/a/div/span/span').click()
@@ -34,6 +34,7 @@ class Google:
         self.driver.find_element_by_xpath(
             '//*[@id="reporting"]/div/div/div/ga-nav-link[3]/div[2]/div/div/ga-nav-link[2]/div[2]/div/div/ga-nav-link[1]/div/report-link/a/div/span/span').click()
         sleep(5)
+        print("opening iframe")
         self.driver.switch_to_frame('galaxyIframe')
         sleep(5)
         self.driver.find_element_by_id('AnnotationDrawer_drawer_button').click()
@@ -44,6 +45,7 @@ class Google:
         self.driver.find_element_by_css_selector("._GANrb textarea").send_keys("hello world")
         sleep(4)
         self.driver.find_element_by_css_selector("._GAjb._GAWn b").click()
+        print("done")
         #self.driver.close()
 
 Google(theuser, thepassword)
